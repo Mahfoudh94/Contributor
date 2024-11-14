9b1577a06bb28bfe45a987ef62f6ea362fab6591
drLacheheb <yacinolach@gmail.com>
2024-11-14 17:06:36 +0100

    Add showCommits method to GithubController for fetching PR commits

--- app/Http/Controllers/GithubController.php
+++ app/Http/Controllers/GithubController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\Github\GetCommitsFromPullRequest;
 use App\Actions\Github\GetReps;
 use App\Actions\Github\GetRepoBranches;
 use App\Actions\Github\GetRepoPullRequests;
+use App\Http\Requests\GetCommitsRequest;
 use App\Http\Requests\RepoPRRequest;
-use Illuminate\Http\Request;
 use Mockery\Exception;
 
 class GithubController extends Controller
@@ -42,6 +43,17 @@ class GithubController extends Controller
         try {
             return GetRepoPullRequests::run($repo, $request->input('owner_id'),$branch);
         } catch (Exception) {
+            return response()->json([
+                'status' => false,
+                'message' => 'problem getting info from this repo'
+            ], 400);
+        }
+    }
+
+    public function showCommits(string $repo, string $pr_id,GetCommitsRequest $request){
+        try {
+            return GetCommitsFromPullRequest::run($request->input('owner_id'),$repo,$pr_id);
+        }catch (Exception) {
             return response()->json([
                 'status' => false,
                 'message' => 'problem getting info from this repo'
