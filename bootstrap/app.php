6064387a3f49d3a19defb103c3a73a75254c677c
drLacheheb <yacinolach@gmail.com>
2024-11-17 18:27:48 +0100

    Register CheckGitHubWriteAccess middleware alias in application bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\CheckGitHubReadAccess;
+use App\Http\Middleware\CheckGitHubWriteAccess;
 use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -20,7 +21,8 @@ return Application::configure(basePath: dirname(__DIR__))
             AddLinkHeadersForPreloadedAssets::class,
         ]);
         $middleware->alias([
-            'github.read' => CheckGitHubReadAccess::class
+            'github.read' => CheckGitHubReadAccess::class,
+            'github.write' => CheckGitHubWriteAccess::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
