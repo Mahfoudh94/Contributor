4bc2c7a9d249d72ac44839c8c3e399ed3b194ddc
drLacheheb <yacinolach@gmail.com>
2024-11-17 18:24:55 +0100

    Add action to create new branches on GitHub

---
+++ app/Actions/Github/CreateBranch.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Actions\Github;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class CreateBranch
+{
+    use AsAction;
+
+    public function handle(string $owner, string $repo, string $taskBranchName)
+    {
+        $githubAccount = auth()->user()->githubAccount;
+
+        // First, get the SHA of the base branch
+        $branchInfo = Http::withToken($githubAccount->github_token)
+            ->get("https://api.github.com/repos/{$owner}/{$repo}/branches/{$taskBranchName}")
+            ->json();
+
+        $sha = $branchInfo['commit']['sha'];
+
+        // Create the new branch based on the base branch's SHA
+        $response = Http::withToken($githubAccount->github_token)
+            ->post("https://api.github.com/repos/{$owner}/{$repo}/git/refs", [
+                'ref' => "refs/heads/{$taskBranchName}",
+                'sha' => $sha
+            ]);
+
+        return $response->json();
+    }
+}
