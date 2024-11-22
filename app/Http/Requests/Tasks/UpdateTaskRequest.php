dde692b9784b8df52d2127667f5582acccbed512
drLacheheb <yacinolach@gmail.com>
2024-11-22 18:17:33 +0100

    Added UpdateTaskRequest for task update validation

---
+++ app/Http/Requests/Tasks/UpdateTaskRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Tasks;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string,ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'sometimes|string|max:255',
+            'description' => 'sometimes|nullable|string',
+            'start_at' => 'sometimes|date|after_or_equal:today',
+        ];
+    }
+}
