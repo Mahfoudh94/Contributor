ea72f217592081f74825e9740b209c2b2f2d468a
drLacheheb <yacinolach@gmail.com>
2024-11-22 23:33:52 +0100

    Add relationships for badges and wallet to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -55,6 +56,15 @@ class User extends Authenticatable
     public function githubAccount(): HasOne
     {
         return $this->hasOne(UserGithubAccount::class, 'user_id');
+    }
+    public function badges(): BelongsToMany
+    {
+        return $this->belongsToMany(Badge::class, 'user_badge');
+    }
+
+    public function wallet(): HasOne
+    {
+        return $this->hasOne(Wallet::class);
     }
 
 }
