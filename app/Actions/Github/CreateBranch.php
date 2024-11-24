7b14b4e6368e9f5a2c37af62fb4c27b104a67ea0
drLacheheb <yacinolach@gmail.com>
2024-11-24 17:01:27 +0100

    Integrate task model and dynamic repository handling

--- app/Actions/Github/CreateBranch.php
+++ app/Actions/Github/CreateBranch.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Github;
 
+use App\Models\Task;
 use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Support\Facades\Http;
 use Lorisleiva\Actions\Concerns\AsAction;
@@ -11,23 +12,26 @@ class CreateBranch
     use AsAction;
 
     /**
+     * @param Task $task
+     * @param string $githubPrivateToken
+     * @return mixed
      * @throws ConnectionException
      */
-    public function handle(string $owner, string $repo, string $taskBranchName)
+    public function handle(Task $task, string $githubPrivateToken): mixed
     {
-        $githubAccount = auth()->user()->githubAccount;
+        $roomRepo = $task->room->repository;
 
         // First, get the SHA of the base branch
-        $branchInfo = Http::withToken($githubAccount->github_token)
-            ->get("https://api.github.com/repos/{$owner}/{$repo}/branches/{$taskBranchName}")
+        $branchInfo = Http::withToken($githubPrivateToken)
+            ->get("https://api.github.com/repos/$roomRepo->owner/$roomRepo->repository/branches/$roomRepo->branch")
             ->json();
 
         $sha = $branchInfo['commit']['sha'];
 
         // Create the new branch based on the base branch's SHA
-        $response = Http::withToken($githubAccount->github_token)
-            ->post("https://api.github.com/repos/{$owner}/{$repo}/git/refs", [
-                'ref' => "refs/heads/{$taskBranchName}",
+        $response = Http::withToken($githubPrivateToken)
+            ->post("https://api.github.com/repos/$roomRepo->owner/$roomRepo->repository/git/refs", [
+                'ref' => "refs/heads/$task->title",
                 'sha' => $sha
             ]);
 
