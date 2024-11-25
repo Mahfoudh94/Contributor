6fc625c3fd52bfc867d8264cde5ce26099fa2685
drLacheheb <yacinolach@gmail.com>
2024-11-25 23:54:57 +0100

    Ensure task user has fork and branch in JoinTask action
    - Check if the user has a fork of the repository and create it if necessary.
    - Check if the task branch exists for the user and create it if necessary.

--- app/Actions/Tasks/JoinTask.php
+++ app/Actions/Tasks/JoinTask.php
@@ -2,6 +2,9 @@
 
 namespace App\Actions\Tasks;
 
+use App\Actions\Github\CheckBranchExists;
+use App\Actions\Github\CheckForkExists;
+use App\Actions\Github\CreateFork;
 use App\Models\Task;
 use App\Models\User;
 use Lorisleiva\Actions\Concerns\AsAction;
@@ -10,10 +13,25 @@ class JoinTask
 {
     use AsAction;
 
-    public function handle(Task $task, User $user)
+    /**
+     * @param Task $task
+     * @param User $user
+     * @param string $githubPrivateToken
+     * @return Task
+     */
+    public function handle(Task $task, User $user, string $githubPrivateToken): Task
     {
+        $roomRepo = $task->room->repository;
+        $userGithubAccount = $user->githubAccount;
+
         // Ensure the user is not already assigned to the task
         if (!$task->users->contains($user)) {
+            if (!CheckForkExists::run($userGithubAccount->account_name, $roomRepo->repository, $githubPrivateToken)) {
+                CreateFork::run($userGithubAccount->account_name, $roomRepo->repository, $githubPrivateToken);
+            }
+            if (!CheckBranchExists::run($userGithubAccount->account_name, $roomRepo->repository, $task->title, $githubPrivateToken)) {
+                CreateTaskBranch::run($userGithubAccount->account_name, $roomRepo, $task->title, $githubPrivateToken);
+            }
             $task->users()->attach($user);
         }
 
