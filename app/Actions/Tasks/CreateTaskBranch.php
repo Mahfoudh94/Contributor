4c09d7c65c7afa7f9e2f5b7de8fea3a1c33cd7d2
drLacheheb <yacinolach@gmail.com>
2024-11-25 23:52:42 +0100

    Add action to create task-specific branch in user account

---
+++ app/Actions/Tasks/CreateTaskBranch.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Actions\Tasks;
+
+use App\Actions\Github\CreateBranch;
+use App\Actions\Github\GetBranchSha;
+use App\Models\RoomRepo;
+use App\Models\Task;
+use App\Models\UserGithubAccount;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class CreateTaskBranch
+{
+    use AsAction;
+
+
+    /**
+     * @param string $githubUsername
+     * @param RoomRepo $roomRepo
+     * @param string $branchName
+     * @param string $githubPrivateToken
+     * @return mixed
+     */
+    public function handle(string $githubUsername, RoomRepo $roomRepo, string $branchName, string $githubPrivateToken): mixed
+    {
+        // First, get the SHA of the base branch
+        $sha = GetBranchSha::run($roomRepo->owner, $roomRepo->repository, $branchName, $githubPrivateToken);
+
+        // Create the new branch based on the task base branch's SHA
+        return CreateBranch::run($githubUsername, $roomRepo->repository, $branchName, $githubPrivateToken, $sha);
+    }
+}
