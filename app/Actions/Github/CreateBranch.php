c3f44ae34b3aaa51f01b73cfe78b17e0daa85ae3
drLacheheb <yacinolach@gmail.com>
2024-11-25 23:48:57 +0100

    decouple CreateBranch with explicit parameters for reusability

--- app/Actions/Github/CreateBranch.php
+++ app/Actions/Github/CreateBranch.php
@@ -2,7 +2,6 @@
 
 namespace App\Actions\Github;
 
-use App\Models\Task;
 use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Support\Facades\Http;
 use Lorisleiva\Actions\Concerns\AsAction;
@@ -11,27 +10,21 @@ class CreateBranch
 {
     use AsAction;
 
+
     /**
-     * @param Task $task
+     * @param string $githubUsername
+     * @param string $repoName
+     * @param string $branchName
      * @param string $githubPrivateToken
+     * @param string $sha
      * @return mixed
      * @throws ConnectionException
      */
-    public function handle(Task $task, string $githubPrivateToken): mixed
+    public function handle(string $githubUsername, string $repoName, string $branchName, string $githubPrivateToken, string $sha): mixed
     {
-        $roomRepo = $task->room->repository;
-
-        // First, get the SHA of the base branch
-        $branchInfo = Http::withToken($githubPrivateToken)
-            ->get("https://api.github.com/repos/$roomRepo->owner/$roomRepo->repository/branches/$roomRepo->branch")
-            ->json();
-
-        $sha = $branchInfo['commit']['sha'];
-
-        // Create the new branch based on the base branch's SHA
         $response = Http::withToken($githubPrivateToken)
-            ->post("https://api.github.com/repos/$roomRepo->owner/$roomRepo->repository/git/refs", [
-                'ref' => "refs/heads/$task->title",
+            ->post("https://api.github.com/repos/$githubUsername/$repoName/git/refs", [
+                'ref' => "refs/heads/$branchName",
                 'sha' => $sha
             ]);
 
